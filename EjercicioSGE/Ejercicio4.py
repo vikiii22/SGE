837e52bb5d9d61e265522ac07f44743991b3f8ce
JoseASL <joseviki10@gmail.com>
2021-10-29 16:48:52 +0200

    ejercicios

--- EjercicioSGE/Ejercicio4.py
+++ EjercicioSGE/Ejercicio4.py
@@ -1,7 +1,7 @@
 nombre = input("Dime tu nombre: ")
 sexo = input("Dime tu sexo (F)emenino/(M)asculino: ")
 
-if (nombre < "M" and sexo=="F") or (nombre < "N" and sexo=="M"):
+if (nombre < "M" and sexo == "F") or (nombre < "N" and sexo == "M"):
     print("Perteneces al grupo A")
 else:
     print("Perteneces al grupo B")
--- EjercicioSGE/Ejercicio6.py
+++ EjercicioSGE/Ejercicio6.py
@@ -1,7 +1,7 @@
 contrasenya="contraseña"
 contrasenyaIntro=input("Introduce la contraseña: ")
 
-while contrasenyaIntro.lower() != "contraseña":
+while contrasenyaIntro.lower() != contrasenya:
     print("Contraseña errónea")
     contrasenyaIntro = input("Introduce la contraseña: ")
 
